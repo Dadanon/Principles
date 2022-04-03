aebf3d4e3761b12a25cdb7ddc56fe62b787d9db4
Dadanon <mar4ello19922@gmail.com>
2022-04-03 13:54:45 +0300

    Добавлено немного по дженерикам

---
+++ Principles/Generics/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Principles.Generics
+{
+    // Обобщенные типы
+    // Для тех полей объекта, наилучший тип для которых мы не придумали
+    // или не знаем, каким он будет при инициализации,
+    // разумно было бы использовать тип object. Но! - упаковка / распаковка - затратно.
+
+    // Поэтому добавлены обобщенные типы.
+    class Person<T>
+    {
+        public T Id { get; set; }
+        public string Name { get; set; }
+        public Person(T id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return $"{Id} {Name}";
+        }
+    }
+    // В данном случае Т - универсальный параметр, т.к.
+    // мы еще не знаем, какой это будет тип.
+
+    // Мы можем представить универсальный параметр обобщенным типом,
+    // т.е. <T> - generic.
+    class Company<P>
+    {
+        public P CEO { get; set; }
+        public Company(P ceo)
+        {
+            CEO = ceo;
+        }
+        public override string ToString()
+        {
+            return $"This company CEO: {CEO}";
+        }
+    }
+
+    // Можно создать отдельные статические поля для разных типов,
+    // например, для int и string.
+    class Man<T>
+    {
+        public static T code;
+        public T Id { get; set; }
+        public string Name { get; set; }
+        public Man(T id, string name)
+        {
+            Name = name;
+            Id = id;
+        }
+        public override string ToString()
+        {
+            return $"{code} {Id} {Name}";
+        }
+    }
+
+    // Также мы можем использовать несколько универсальных параметров.
+    class Women<P, T>
+    {
+        public P Id { get; }
+        public T Password { get; set; }
+        public string WomenName { get; }
+        public Women(P id, T password, string womenName)
+        {
+            Id = id;
+            Password = password;
+            WomenName = womenName;
+        }
+        public override string ToString()
+        {
+            return $"{Id} {Password} {WomenName}";
+        }
+    }
+}
--- Principles/Program.cs
+++ Principles/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Principles.Abstraction.Level_1;
 using Principles.Abstraction.Level_2;
+using Principles.Generics;
 
 namespace Principles
 {
@@ -8,11 +9,30 @@ namespace Principles
     {
         static void Main(string[] args)
         {
-            Truck truck = new Truck();
-            truck.GetStats();
-            Console.WriteLine("");
-            Car car = new Car();
-            car.GetStats();
+            Person<string> bob = new Person<string>("522", "Bob");
+            Console.WriteLine(bob);
+            Company<Person<string>> company = new Company<Person<string>>(bob);
+            Console.WriteLine(company);
+
+            Man<int> tom = new Man<int>(566, "Tom");
+            Man<string>.code = "bulk";
+            Man<int>.code = 15;
+            Console.WriteLine(tom);
+
+            Women<int, string> marfa = new Women<int, string>(509, "glupoe", "Marfa");
+            Console.WriteLine(marfa);
+
+            // Наконец мы можем использовать обобщенные методы,
+            // принимающие значение неопределенного типа.
+            Swap<int>(5, 3);
+
+            void Swap<T>(T x, T y)
+            {
+                (x, y) = (y, x);
+                Console.WriteLine($"x: {x}, y: {y}");
+            }
+            // Подобный обмен можно делать через кортежи (как тут),
+            // можно через временную переменную, XOR.
         }
     }
 }
